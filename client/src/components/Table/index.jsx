c692ddeb545b397eb22426711983a05b59f0e6f3
RianVitor26 <rianvitorlhe@gamil.com>
2023-06-06 21:18:59 -0300

    feat: desenvolvendo a lógica para registros não encontrados

--- client/src/components/Table/index.jsx
+++ client/src/components/Table/index.jsx
@@ -1,68 +1,78 @@
+import { useState } from "react";
 import { Table, TableContainer, Td, Th, Thead, Tr } from "./styles";
 
 export const TableComponent = () => {
+
+  const [hasRegister, setHasRegister] = useState(true)
+
   return (
-    <TableContainer>
-      <Table>
-        <Thead>
-          <Tr>
-            <Th>ID</Th>
-            <Th>Nome</Th>
-            <Th>CG</Th>
-            <Th>Peso</Th>
-            <Th>teste</Th>
-            <Th>teste</Th>
-          </Tr>
-        </Thead>
-        <Tr>
-          <Td>Lorem, ipsum dolor.</Td>
-          <Td>Lorem, ipsum dolor.</Td>
-          <Td>Lorem, ipsum dolor.</Td>
-          <Td>Lorem, ipsum dolor.</Td>
-          <Td>Lorem, ipsum dolor.</Td>
-          <Td>Lorem, ipsum dolor.</Td>
-        </Tr>
-        <Tr>
-          <Td>Lorem, ipsum dolor.</Td>
-          <Td>Lorem, ipsum dolor.</Td>
-          <Td>Lorem, ipsum dolor.</Td>
-          <Td>Lorem, ipsum dolor.</Td>
-          <Td>Lorem, ipsum dolor.</Td>
-          <Td>Lorem, ipsum dolor.</Td>
-        </Tr>
-        <Tr>
-          <Td>Lorem, ipsum dolor.</Td>
-          <Td>Lorem, ipsum dolor.</Td>
-          <Td>Lorem, ipsum dolor.</Td>
-          <Td>Lorem, ipsum dolor.</Td>
-          <Td>Lorem, ipsum dolor.</Td>
-          <Td>Lorem, ipsum dolor.</Td>
-        </Tr>
-        <Tr>
-          <Td>Lorem, ipsum dolor.</Td>
-          <Td>Lorem, ipsum dolor.</Td>
-          <Td>Lorem, ipsum dolor.</Td>
-          <Td>Lorem, ipsum dolor.</Td>
-          <Td>Lorem, ipsum dolor.</Td>
-          <Td>Lorem, ipsum dolor.</Td>
-        </Tr>
-        <Tr>
-          <Td>Lorem, ipsum dolor.</Td>
-          <Td>Lorem, ipsum dolor.</Td>
-          <Td>Lorem, ipsum dolor.</Td>
-          <Td>Lorem, ipsum dolor.</Td>
-          <Td>Lorem, ipsum dolor.</Td>
-          <Td>Lorem, ipsum dolor.</Td>
-        </Tr>
-        <Tr>
-          <Td>Lorem, ipsum dolor.</Td>
-          <Td>Lorem, ipsum dolor.</Td>
-          <Td>Lorem, ipsum dolor.</Td>
-          <Td>Lorem, ipsum dolor.</Td>
-          <Td>Lorem, ipsum dolor.</Td>
-          <Td>Lorem, ipsum dolor.</Td>
-        </Tr>
-      </Table>
-    </TableContainer>
+    <>
+      {hasRegister === false ? (
+        <h1>Nenhum registro encontrado</h1>
+      ) : (
+        <TableContainer>
+          <Table>
+            <Thead>
+              <Tr>
+                <Th>ID</Th>
+                <Th>Nome</Th>
+                <Th>CG</Th>
+                <Th>Peso</Th>
+                <Th>teste</Th>
+                <Th>teste</Th>
+              </Tr>
+            </Thead>
+            <Tr>
+              <Td>Lorem, ipsum dolor.</Td>
+              <Td>Lorem, ipsum dolor.</Td>
+              <Td>Lorem, ipsum dolor.</Td>
+              <Td>Lorem, ipsum dolor.</Td>
+              <Td>Lorem, ipsum dolor.</Td>
+              <Td>Lorem, ipsum dolor.</Td>
+            </Tr>
+            <Tr>
+              <Td>Lorem, ipsum dolor.</Td>
+              <Td>Lorem, ipsum dolor.</Td>
+              <Td>Lorem, ipsum dolor.</Td>
+              <Td>Lorem, ipsum dolor.</Td>
+              <Td>Lorem, ipsum dolor.</Td>
+              <Td>Lorem, ipsum dolor.</Td>
+            </Tr>
+            <Tr>
+              <Td>Lorem, ipsum dolor.</Td>
+              <Td>Lorem, ipsum dolor.</Td>
+              <Td>Lorem, ipsum dolor.</Td>
+              <Td>Lorem, ipsum dolor.</Td>
+              <Td>Lorem, ipsum dolor.</Td>
+              <Td>Lorem, ipsum dolor.</Td>
+            </Tr>
+            <Tr>
+              <Td>Lorem, ipsum dolor.</Td>
+              <Td>Lorem, ipsum dolor.</Td>
+              <Td>Lorem, ipsum dolor.</Td>
+              <Td>Lorem, ipsum dolor.</Td>
+              <Td>Lorem, ipsum dolor.</Td>
+              <Td>Lorem, ipsum dolor.</Td>
+            </Tr>
+            <Tr>
+              <Td>Lorem, ipsum dolor.</Td>
+              <Td>Lorem, ipsum dolor.</Td>
+              <Td>Lorem, ipsum dolor.</Td>
+              <Td>Lorem, ipsum dolor.</Td>
+              <Td>Lorem, ipsum dolor.</Td>
+              <Td>Lorem, ipsum dolor.</Td>
+            </Tr>
+            <Tr>
+              <Td>Lorem, ipsum dolor.</Td>
+              <Td>Lorem, ipsum dolor.</Td>
+              <Td>Lorem, ipsum dolor.</Td>
+              <Td>Lorem, ipsum dolor.</Td>
+              <Td>Lorem, ipsum dolor.</Td>
+              <Td>Lorem, ipsum dolor.</Td>
+            </Tr>
+          </Table>
+        </TableContainer>
+      )}
+    </>
   );
 };
